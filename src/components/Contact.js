24613b0c8d0ec14529132422cb51987f7b0457d1
ChaseTramel <chasetramel@hey.com>
2023-12-19 14:47:52 -0500

    Created contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-import { Card, Typography } from "@mui/material";
+import { Box, Button, Card, Typography } from "@mui/material";
 
 
 
@@ -22,10 +22,30 @@ function Contact() {
     ]
     
     return (
-        <Card  id="contact" sx={{padding: 2, marginTop: 3, borderRadius: "10px"}}>
+        <Card  id="contact" sx={{padding: 2, marginTop: 3, borderRadius: "10px",}}>
             <Typography variant="h2" sx={{fontSize: 40, fontFamily: "Fira Sans"}}>
                 Contact
             </Typography>
+            <Box sx={{ display: "flex", justifyContent: "space-around" }}>
+                {contactInfo.map((item, index) => (
+                    <Button
+                        key={index}
+                        variant="contained"
+                        href={item.url}
+                        startIcon={<img src={item.icon} alt={item.label} width={16} />}
+                        sx={{
+                            margin: "30px",
+                            bgcolor: "#9ca0b0",
+                            color: "white",
+                            fontFamily: "Fira Sans",
+                            fontWeight: 800,
+                            '&:hover': { bgcolor: "#7287fd", color: "white" }
+                        }}
+                    >
+                        {item.label}
+                    </Button>
+                ))}
+            </Box>
         </Card>
     )
 }
